b3258a75527692d50a2edc2fb37e28e6c0c54c1e
Ubuntu <ubuntu@ip-172-31-34-129.ec2.internal>
2019-10-08 06:04:39 +0000

    preliminary data retrieval from weather api

--- scripts/script.js
+++ scripts/script.js
@@ -5,43 +5,57 @@
 //             });
 
 
-function showData() {
-//     var data = {
-//         resource_id: '2e60a190-211b-4564-b722-24d92778b886', // the resource id
-//         limit: 5, // get 5 results
-//         q: 'jones' // query for 'jones'
-//     };
-//     $.ajax({
-//         url: 'https://data.gov.sg/api/action/datastore_search',
-//         data: data,
-//         dataType: 'jsonp',
-//         success: function(data) {
-//             alert('Total results found: ' + data.result.total)
-//         }
-//     });
-    
-    
-    // axios.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + pos.lat + "," + pos.lng + "&key=AIzaSyDoEv5jmhi5Iw1bPJTBGAEAWUsS-BQnBro")
-    //         .then(function(response) {
-    //             let result = response.data.results[0].formatted_address;
-    //             $("#search-location").val(result);
-    //         });
+$("#show-carpark-data").on("click", function() {
+
+    // alert("This works!")
+    showCarparkData()
+
+});
+
+
+
+$("#show-weather-data").on("click", function() {
+
+    // alert("Weather works!")
+    showWeatherData()
+
+});
+
+
+
+
+function showCarparkData() {
     
 
     axios.get("https://api.data.gov.sg/v1/transport/carpark-availability")
             .then(function(response) {
                 let result = response
-                console.log(response)
-                alert("This works!")
+                console.log(result)
+                let testName=
+                alert("Car works!")
             });
 
 };
 
 
+function showWeatherData() {
+    
 
-$("#show-data").on("click", function() {
+    axios.get("https://api.data.gov.sg/v1/environment/air-temperature")
+            .then(function(response) {
+                // let result = response.data.metadata
+                console.log(response)
+                
+                let stations = response.data.metadata.stations
+                let readings = response.data.items[0].readings
+                
+                console.log(readings)
+                console.log(stations)
+                for (var i=0; i<stations.length; i++){
+                    $("#weather-chart").append("Station Name: " +  stations[i].name + "Temperature: " + readings[i].value)
+                }
+                alert("Weather works!")
+            });
 
-    // alert("This works!")
-    showData()
+};
 
-});
