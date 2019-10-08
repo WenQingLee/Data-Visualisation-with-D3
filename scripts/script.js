bd1b8fd25671e797309a6164525945015f5f0cb6
Ubuntu <ubuntu@ip-172-31-34-129.ec2.internal>
2019-10-08 07:10:58 +0000

    added a pie chart

--- scripts/script.js
+++ scripts/script.js
@@ -52,11 +52,19 @@ function showWeatherData() {
             console.log(readings)
             console.log(stations)
             
+            // Initialising the new data array
             let newDataArray = []
             
             for (var i = 0; i < stations.length; i++) {
-                $("#weather-chart").append("Station Name: " + stations[i].name + "Temperature: " + readings[i].value)
                 
+                 let num = i + 1;
+
+            $("#display-weather-results").append("<tr><td>" + num + "</td>" + "<td>" + stations[i].name + "</td>" + "<td>" + readings[i].value + "</td></tr>");
+                
+                
+                $("#weather-chart").append("Station Name: " + stations[i].name + "Temperature: " + readings[i].value +"<br />")
+                
+                // Creating an object to add into the new data array
                 let addData = {
                     "name": stations[i].name,
                     "value": readings[i].value,
@@ -64,6 +72,7 @@ function showWeatherData() {
                 
                 console.log(addData)
                 
+                // Pushing the onjects into the array
                 newDataArray.push(addData);
                 // newDataArray = newDataArray.push({"name": stations[i].name, "value": readings[i].value})
                 // newDataArray = newDataArray.push(readings[i].value)
@@ -79,18 +88,28 @@ function showWeatherData() {
             var ndx = crossfilter(newDataArray);
             var name_dim = ndx.dimension(dc.pluck('name'));
             var temperature_dim = name_dim.group().reduceSum(dc.pluck('value'))
-
+            
+            // Bar Chart
             dc.barChart('#weather-chart')
-                .width(300)
-                .height(150)
+                .width(1500)
+                .height(500)
                 .margins({ top: 10, right: 50, bottom: 30, left: 50 })
                 .dimension(name_dim)
                 .group(temperature_dim)
                 .transitionDuration(500)
                 .x(d3.scale.ordinal())
                 .xUnits(dc.units.ordinal)
-                .xAxisLabel("Person")
+                .xAxisLabel("Station Name")
+                .yAxisLabel("Temperature")
                 .yAxis().ticks(4);
+                
+                // Pie Chart
+            dc.pieChart('#weather-pie-chart')
+                .height(600)
+                .radius(600)
+                .transitionDuration(1500)
+                .dimension(name_dim)
+                .group(temperature_dim);
 
             dc.renderAll();
 
