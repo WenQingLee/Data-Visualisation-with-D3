38b927b4cb929bccb1a482333a500403774271dc
Ubuntu <ubuntu@ip-172-31-34-129.ec2.internal>
2019-10-09 12:34:15 +0000

    created a stacked chart based on prof ranks

--- CSV/js/script.js
+++ CSV/js/script.js
@@ -7,8 +7,8 @@ function createGraph(error, salaryData) {
     var ndx = crossfilter(salaryData)
 
     // To convert the salaries to integers otherwise it will not work in averageSalary function
-    salaryData.forEach(function(d){
-        d.salary=parseInt(d.salary)
+    salaryData.forEach(function(d) {
+        d.salary = parseInt(d.salary)
     })
 
 
@@ -20,6 +20,9 @@ function createGraph(error, salaryData) {
 
     // To show the average salary
     averageSalary(ndx)
+    
+    // To show the rank type distribution
+    rankType(ndx)
 
     dc.renderAll();
 
@@ -70,7 +73,7 @@ function averageSalary(ndx) {
     // The reduce function in crossfilter requires to define 3 functions, add, remove and initialisation
     // To change the default reduce function in crossfilter, the add and remove functions must be updated
 
-    // Updated add function where p is the incremental and v is the value
+    // Updated add function where p is the accumulator and v is the value added
     function addSalary(p, v) {
         p.count++;
         p.total += v.salary;
@@ -118,5 +121,68 @@ function averageSalary(ndx) {
         .elasticY(true)
         .xAxisLabel("Gender")
         .yAxis().ticks(4);
+
+}
+
+
+// To show the graph breakdown by rank
+function rankType(ndx) {
+
+    var dim = ndx.dimension(dc.pluck("sex"));
+
+    // Higher order functions to sort the different ranks
+    var profByGender = rankByGender(dim, "Prof");
+    var asstProfByGender = rankByGender(dim, "AsstProf");
+    var assocProfByGender = rankByGender(dim, "AssocProf");
+
+    // Supporting function for the higher order function using a custom reduce function
+
+    function rankByGender(dimension, rank) {
+        return dimension.group().reduce(
+            // add function
+            function(p, v) {
+                p.total++;
+                if (v.rank == rank) {
+                    p.match++;
+                }
+                return p;
+            },
+            // reduce function
+            function(p, v) {
+                p.total--;
+                if (v.rank == rank) {
+                    p.match--;
+                }
+                return p;
+            },
+            // initialiser
+            function() {
+                return { total: 0, match: 0 };
+            }
+        );
+    }
+    
+    // To show the rank distribution curve. note the stacked graphs and margins to the right for the legend
+    // The value accessor determines the percentage
+    dc.barChart("#rank-type")
+        .width(400)
+        .height(300)
+        .dimension(dim)
+        .group(profByGender, "Prof")
+        .stack(asstProfByGender, "Asst Prof")
+        .stack(assocProfByGender, "Assoc Prof")
+        .valueAccessor(function(d) {
+            if(d.value.total > 0) {
+                return (d.value.match / d.value.total) * 100;
+            } else {
+                return 0;
+            }
+        })
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
+        .margins({top: 10, right: 100, bottom: 30, left: 30});
+
+
 
 }
